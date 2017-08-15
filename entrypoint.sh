915a4190df64ff0ecf54b5a4fb23dbcff752a63e
fsword <li.jianye@gmail.com>
2017-08-15 18:03:07 +0800

    不再检查cert file，避免误导

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,7 @@
 
 set -e
 
-CERT_FILE="/etc/docker/registry/cert.pem"
 CONFIG_FILE="/etc/docker/registry/config.yml"
-if [ ! -f "$CERT_FILE" ]
-then
-  echo "Cannot find $CERT_FILE" >&2
-  exit 1
-fi
 
 cat /config.yml.template \
   | sed "s/Bearer/Bearer ${SERVICE_TOKEN}/" \
